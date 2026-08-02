457b60feedd40425fa1135d6885ed5257419dfeb
brad beer <bradwbeer@gmail.com>
2026-08-02 15:47:42 -0400

    changed macro to mac.

--- printer.c
+++ printer.c
@@ -246,7 +246,7 @@ static void stringify_lambda(resizable_string_type* buf, void* o, int base) {
 }
 
 static void stringify_macro(resizable_string_type* buf, void* o, int base) {
-  putstr_resizable_array(buf, "(MACRO ");
+  putstr_resizable_array(buf, "(MAC ");
   stringify_dispatch(buf, car(cdr(o)), base);
   stringify_body_forms(buf, cdr(cdr(o)), base);
   putch_resizable_array(buf, ')');
